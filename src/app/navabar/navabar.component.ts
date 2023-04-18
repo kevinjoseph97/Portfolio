b6b4f332736a056864b5bfcb352e3db81661ff53
Kevin Joseph <kevinjoseph97@gmail.com>
2023-04-18 20:42:31 +0200

    adds navbar

---
+++ src/app/navabar/navabar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navabar',
+  templateUrl: './navabar.component.html',
+  styleUrls: ['./navabar.component.css']
+})
+export class NavabarComponent {
+
+}
